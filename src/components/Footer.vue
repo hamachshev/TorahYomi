53b01a4fb93ddc2870333b6ff0456d25af2d8c2a
hamachshev <41763577+hamachshev@users.noreply.github.com>
2022-05-06 14:52:57 -0400

    changed footer to sans and centered and added padding

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer">
-    <p>Sponsered in the memory of Gershon Yosef Ben Yisrael Moshe</p>
+    <p>Sponsored in the memory of Gershon Yosef Ben Yisrael Moshe</p>
   </div>
 </template>
 <style scoped>
@@ -15,5 +15,8 @@
   background-color: #01adee;
   margin: 0;
   padding: 0;
+  font-family: sans-serif;
+  padding: 4px;
+  justify-content: center;
 }
 </style>
