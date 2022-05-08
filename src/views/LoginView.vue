162b432701a3fc40b299ba0ab6fd326f1e137007
Shlomo Kalinsky <93456838+sdkkds2125@users.noreply.github.com>
2022-05-08 00:13:27 -0400

    frontend validation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -42,6 +42,52 @@ export default {
           const errorMessage = error.message;
           console.log(errorMessage);
         });
+      // validate the first name entry
+      if (this.first_name === "") {
+        this.fNameMessage = "This field is required.";
+        this.isValid = false;
+      } else {
+        this.fNameMessage = "";
+      }
+      //last name check
+      if (this.last_name === "") {
+        this.lNameMessage = "This field is required.";
+        this.isValid = false;
+      } else {
+        this.lNameMessage = "";
+      }
+      //email1 check
+      if (this.email1 === "") {
+        this.email1Message = "This field is required.",
+            this.isValid = false
+      } else if ( !this.emailPattern.test(this.email1) ) {
+        this.email1Message = "Must be a valid email address.",
+            this.isValid = false
+      } else {
+        this.email1Message = ""
+      }
+      // validate the second email entry
+      if (this.email2 === "") {
+        this.email2Message = "This field is required.";
+        this.isValid = false;
+      } else if (this.email2 !== this.email1 ) {
+        this.email2Message = "Must equal first email entry.";
+        this.isValid = false;
+      } else {
+        this.email2Message = "";
+      }
+      if (this.password === "") {
+        this.passMessage = "This field is required.";
+        this.isValid = false;
+      }else if (this.password.length < 6){
+        this.passMessage = "Password must be at least six characters";
+        this.isValid = false;
+      } else {
+        this.passMessage = "";
+      }
+      if (this.isValid === false) {
+        event.preventDefault()
+      }
       this.redirectIfSignedIn();
     },
     redirectIfSignedIn() {
@@ -123,8 +169,9 @@ export default {
           <span>{{ passMessage }}</span>
         </div>
       </div>
-
+      <div class="row">
       <button class="btn btn-primary btn-md w-50" type="submit">Login</button>
+      </div>
     </form>
   </div>
 </template>
